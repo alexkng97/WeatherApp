d6ec4e0ff6042be74a7fdb2b08c39cc9189328ca
Alex Ng <alexkng97@gmail.com>
2021-01-28 10:14:37 +0000

    beginning on connection manager

--- WeatherAPI/src/main/java/com/sparta/alex/controller/ConnectionManager.java
+++ WeatherAPI/src/main/java/com/sparta/alex/controller/ConnectionManager.java
@@ -1,4 +1,27 @@
 package com.sparta.alex.controller;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ConnectionManager {
+
+	private HttpClient httpClient;
+	private HttpRequest httpRequest;
+	private HttpResponse httpResponse;
+
+	public ConnectionManager(){ }
+
+	public void connectToApi(String url){
+		try {
+			httpClient = HttpClient.newHttpClient();
+			httpRequest = HttpRequest.newBuilder().uri(new URI(url)).build();
+			httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
