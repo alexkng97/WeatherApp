f2176a6040dc470337fa58a7f7804f968fd584f2
Alex Ng <alexkng97@gmail.com>
2021-01-30 18:33:33 +0000

    code clean up

--- WeatherAPI/src/main/java/com/sparta/alex/view/UserInterface.java
+++ WeatherAPI/src/main/java/com/sparta/alex/view/UserInterface.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class UserInterface {
 
-	public static String getLocation(){
+	public static String getLocation() {
 		System.out.println("Enter location:");
 		Scanner scanner = new Scanner(System.in);
 		return scanner.nextLine();
@@ -14,13 +14,14 @@ public class UserInterface {
 
 
 	public static int chooseLocation(LocationDTO[] locations) {
-		System.out.printf("There are %d locations returned from your search. \n",locations.length);
+		System.out.printf("There are %d locations returned from your search. \n", locations.length);
 		System.out.println("Please choose the location you want: ");
-		for(int i = 0; i < locations.length; i++){
+
+		for (int i = 0; i < locations.length; i++) {
 			System.out.printf("[%d] %s \n", (i + 1), locations[i].getTitle());
 		}
 		Scanner scanner = new Scanner(System.in);
-		return (scanner.nextInt() -1);
+		return (scanner.nextInt() - 1);
 
 	}
 }
